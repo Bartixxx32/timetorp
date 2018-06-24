e1c61625b832b26dda3b050c8a97e203e32ac0e6
Bartixxx <mcbplay1@gmail.com>
2018-06-24 16:37:34 +0200

    mandaty

---
+++ mandaty/mandaty.sql
@@ -0,0 +1,73 @@
+-- --------------------------------------------------------
+-- Host:                         mysql-mariadb-4-101.zap-hosting.com
+-- Wersja serwera:               10.1.29-MariaDB-1~jessie - mariadb.org binary distribution
+-- Serwer OS:                    debian-linux-gnu
+-- HeidiSQL Wersja:              9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Zrzucanie danych dla tabeli zap352658-1.fine_types: ~52 rows (około)
+/*!40000 ALTER TABLE `fine_types` DISABLE KEYS */;
+REPLACE INTO `fine_types` (`id`, `label`, `amount`, `category`) VALUES
+	(1, 'Misuse of a horn', 30, 0),
+	(2, 'Illegally Crossing a continuous Line', 40, 0),
+	(3, 'Driving on the wrong side of the road', 250, 0),
+	(4, 'Illegal U-Turn', 250, 0),
+	(5, 'Illegally Driving Off-road', 170, 0),
+	(6, 'Refusing a Lawful Command', 30, 0),
+	(7, 'Illegally Stoped of a Vehicle', 150, 0),
+	(8, 'Illegal Parking', 70, 0),
+	(9, 'Failing to Yield to the right', 70, 0),
+	(10, 'Failure to comply with Vehicle Information', 90, 0),
+	(11, 'Failing to stop at a Stop Sign ', 105, 0),
+	(12, 'Failing to stop at a Red Light', 130, 0),
+	(13, 'Illegal Passing', 100, 0),
+	(14, 'Driving an illegal Vehicle', 100, 0),
+	(15, 'Driving without a License', 1500, 0),
+	(16, 'Hit and Run', 800, 0),
+	(17, 'Exceeding Speeds Over < 5 mph', 90, 0),
+	(18, 'Exceeding Speeds Over 5-15 mph', 120, 0),
+	(19, 'Exceeding Speeds Over 15-30 mph', 180, 0),
+	(20, 'Exceeding Speeds Over > 30 mph', 300, 0),
+	(21, 'Impeding traffic flow', 110, 1),
+	(22, 'Public Intoxication', 90, 1),
+	(23, 'Disorderly conduct', 90, 1),
+	(24, 'Obstruction of Justice', 130, 1),
+	(25, 'Insults towards Civilans', 75, 1),
+	(26, 'Disrespecting of an LEO', 110, 1),
+	(27, 'Verbal Threat towards a Civilan', 90, 1),
+	(28, 'Verbal Threat towards an LEO', 150, 1),
+	(29, 'Providing False Information', 250, 1),
+	(30, 'Attempt of Corruption', 1500, 1),
+	(31, 'Brandishing a weapon in city Limits', 120, 2),
+	(32, 'Brandishing a Lethal Weapon in city Limits', 300, 2),
+	(33, 'No Firearms License', 600, 2),
+	(34, 'Possession of an Illegal Weapon', 700, 2),
+	(35, 'Possession of Burglary Tools', 300, 2),
+	(36, 'Grand Theft Auto', 1800, 2),
+	(37, 'Intent to Sell/Distrube of an illegal Substance', 1500, 2),
+	(38, 'Frabrication of an Illegal Substance', 1500, 2),
+	(39, 'Possession of an Illegal Substance ', 650, 2),
+	(40, 'Kidnapping of a Civilan', 1500, 2),
+	(41, 'Kidnapping of an LEO', 2000, 2),
+	(42, 'Robbery', 650, 2),
+	(43, 'Armed Robbery of a Store', 650, 2),
+	(44, 'Armed Robbery of a Bank', 1500, 2),
+	(45, 'Assault on a Civilian', 2000, 3),
+	(46, 'Assault of an LEO', 2500, 3),
+	(47, 'Attempt of Murder of a Civilian', 3000, 3),
+	(48, 'Attempt of Murder of an LEO', 5000, 3),
+	(49, 'Murder of a Civilian', 10000, 3),
+	(50, 'Murder of an LEO', 30000, 3),
+	(51, 'Involuntary manslaughter', 1800, 3),
+	(52, 'Fraud', 2000, 2);
+/*!40000 ALTER TABLE `fine_types` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
